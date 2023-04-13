ba6def45f7a170396e59588c77d76f9a4c9dc00b
gaurav chaudhari <gauravchaudhari7717@gmail.com>
2023-04-13 23:08:13 +0530

    render date and time funtion done

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,1 +1,15 @@
 // todo 
+window.addEventListener("load", function() {
+    setInterval(() => {
+    const{day,time} = renderCurrentDateTime();
+    console.log(day);
+    console.log(time);
+
+    let hederDay = this.document.getElementById("day");
+    let hederDate = this.document.getElementById("date");
+
+    hederDay.innerText = day;
+    hederDate.innerText = time;
+  });
+  
+},60000);
