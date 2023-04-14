d3ca519a020ea5cce494dbd86ffe2b3965b5919f
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-04-14 20:36:04 +0530

    exclude edit-menu child elements
    exclude all the child elemets of edit-menu onClick to affect on section click for toggle off

--- scripts/components/edit_menu.js
+++ scripts/components/edit_menu.js
@@ -2,24 +2,25 @@ const editIcon = document.querySelector(".edit-icon");
 const editIconImg = document.querySelector("#edit-icon-img");
 const editMenu = document.querySelector(".edit-menu");
 const mainContainer = document.getElementById("main");
+const updateJSONBtn = document.getElementById('update-json-btn')
+const downloadTemplateBtn = document.getElementById('download-template-btn');
 let menuToggle = false;
 
 // handle edit-icon onClick
 editIcon.addEventListener("click", () => {
-  if (!menuToggle) {
-    editMenuToggleOn();
-  } else {
-    editMenuToggleOff();
-  }
+    if (!menuToggle) {
+        editMenuToggleOn();
+    } else {
+        editMenuToggleOff();
+    }
 
-  menuToggle = !menuToggle;
+    menuToggle = !menuToggle;
 });
 
-document.addEventListener("click", function (event) {
-  console.log(event.target);
-  if (event.target != editIconImg) {
-    console.log("hello");
-    editMenuToggleOff();
-    menuToggle = !menuToggle;
-  }
+document.addEventListener("click", function (e) {
+    console.log(e.target);
+    if (e.target != editIconImg && !editMenu.contains(e.target)) {
+        editMenuToggleOff();
+        menuToggle = !menuToggle;
+    }
 });
